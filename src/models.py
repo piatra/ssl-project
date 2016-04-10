85542282162158e9e3d0ff339ee67d7809fb2a3c
tiriplicamihai <tiriplica.mihai@gmail.com>
2016-04-10 17:14:59 +0300

    Add model for storing words.

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, Column, Float, Integer, String
+from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.engine.url import URL
 
@@ -13,9 +14,11 @@ def db_connect():
     """
     return create_engine(URL(**settings.DATABASE))
 
-def create_website_table(engine):
+
+def create_db_tables(engine):
     """"""
     DeclarativeBase.metadata.create_all(engine)
+
 
 class Websites(DeclarativeBase):
     """Sqlalchemy websites model"""
@@ -25,3 +28,12 @@ class Websites(DeclarativeBase):
     link = Column('link', String, nullable=True)
     male_ratio = Column('male_ratio', Float, nullable=True)
     female_ratio = Column('female_ratio', Float, nullable=True)
+
+
+class WebsitesContent(DeclarativeBase):
+    """Sqlalchemy websites model"""
+    __tablename__ = "websites_content"
+
+    id = Column(Integer, primary_key=True)
+    link = Column('link', String, nullable=False)
+    words = Column('words', ARRAY(String), nullable=False)

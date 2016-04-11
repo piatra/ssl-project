b69e7e158fd6af2dda815fcb91679a4ac9eedac7
Andrei Oprea <andrei.br92@gmail.com>
2016-04-11 17:23:08 +0300

    Add alexa and quantcast labels to fields

--- src/demographic_scraper/demographic_scraper/items.py
+++ src/demographic_scraper/demographic_scraper/items.py
@@ -2,5 +2,7 @@ import scrapy
 
 class DemographicScraperItem(scrapy.Item):
     link = scrapy.Field()
-    male_ratio = scrapy.Field()
-    female_ratio = scrapy.Field()
+    male_ratio_alexa = scrapy.Field()
+    female_ratio_alexa = scrapy.Field()
+    male_ratio_quantcast = scrapy.Field()
+    female_ratio_quantcast = scrapy.Field()

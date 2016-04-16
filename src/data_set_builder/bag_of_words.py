290bb94a9a70cbf2fbc1c558257f8fca5b835baf
tiriplicamihai <tiriplica.mihai@gmail.com>
2016-04-16 13:29:34 +0300

    Train and use classifier.

--- src/data_set_builder/bag_of_words.py
+++ src/data_set_builder/bag_of_words.py
@@ -1,5 +1,15 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
+from sqlalchemy.orm import sessionmaker
+
+from models import create_db_tables, db_connect, WebsitesContent, Websites
+from data_set_builder import extract_words_from_url
+
+CLASSES = {
+    'male': 0,
+    'female': 1,
+}
+
 
 def bag_of_words(clean_train_reviews):
     """
@@ -17,10 +27,12 @@ def bag_of_words(clean_train_reviews):
     train_data_features = train_data_features.toarray()
     return vectorizer, train_data_features
 
+
 def forrest_classifier(train_data_features, classes):
     forest = RandomForestClassifier(n_estimators = 100)
 
     return forest.fit(train_data_features, classes)
+
 
 def classify(training_data, training_classes, test_data):
     vectorizer, features_train_data = bag_of_words(training_data)
@@ -29,8 +41,40 @@ def classify(training_data, training_classes, test_data):
     return classifier.predict(vectorizer.transform(test_data))
 
 
-# Example usage
-# training_data = ["ana are mere", "gigi are pere", "stefan are mere", "cristi are pere"]
-# training_classes = [0, 1, 0, 1]
-# test_data = ["nicu are mere", "alex are pere"]
-# print classify(training_data, training_classes, test_data)
+def get_history():
+    return ['https://www.cars.com']
+
+
+def main():
+    """Build training set and clasify history. """
+    engine = db_connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    training_set = []
+    labels = []
+    for website in session.query(Websites).all():
+        website_content = session.query(WebsitesContent).filter(
+            WebsitesContent.link==website.link)[0]
+        words = website_content.words
+        words = ' '.join(words)
+
+        gender = CLASSES['male']
+        if website.male_ratio_alexa < website.female_ratio_alexa:
+            gender = CLASSES['female']
+
+        training_set.append(words)
+        labels.append(gender)
+
+    test_set = []
+    for url in get_history():
+        words = extract_words_from_url(url)
+        words = ' '.join(words)
+
+        test_set.append(words)
+
+    print classify(training_set, labels, test_set)
+
+
+if __name__ == '__main__':
+    main()

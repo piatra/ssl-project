491d07bc88f54cc676fa4de08bfa04ff71eee067
Andrei Oprea <andrei.br92@gmail.com>
2016-04-24 19:48:49 +0300

    Test classifier accuracy. Currently between 68-70%.

--- src/data_set_builder/bag_of_words.py
+++ src/data_set_builder/bag_of_words.py
@@ -2,7 +2,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sqlalchemy.orm import sessionmaker
 
-from models import create_db_tables, db_connect, WebsitesContent, Websites
+from models import db_connect, WebsitesContent, Websites
 from data_set_builder import extract_words_from_url
 
 CLASSES = {
@@ -17,11 +17,12 @@ def bag_of_words(clean_train_reviews):
         The method ASSUMES SENTENCES BEEN CLEANED FOR STOP WORDS.
     """
 
-    vectorizer = CountVectorizer(analyzer = "word",   \
-                                 tokenizer = None,    \
-                                 preprocessor = None, \
-                                 stop_words = None,   \
-                                 max_features = 2000)
+    vectorizer = CountVectorizer(analyzer = "word",    \
+                                 tokenizer = None,     \
+                                 ngram_range = (1, 3), \
+                                 preprocessor = None,  \
+                                 stop_words = None,    \
+                                 max_features = 600)
 
     train_data_features = vectorizer.fit_transform(clean_train_reviews)
     train_data_features = train_data_features.toarray()
@@ -29,16 +30,19 @@ def bag_of_words(clean_train_reviews):
 
 
 def forrest_classifier(train_data_features, classes):
-    forest = RandomForestClassifier(n_estimators = 100)
+    forest = RandomForestClassifier()
 
     return forest.fit(train_data_features, classes)
 
 
-def classify(training_data, training_classes, test_data):
+def classify(training_data, training_classes, test_data, test_data_classes):
     vectorizer, features_train_data = bag_of_words(training_data)
     classifier = forrest_classifier(features_train_data, training_classes)
 
-    return classifier.predict(vectorizer.transform(test_data))
+    if len(test_data_classes) is 0:
+        return classifier.predict(vectorizer.transform(test_data))
+    else:
+        print classifier.score(vectorizer.transform(test_data), test_data_classes)
 
 
 def get_history():
@@ -53,9 +57,11 @@ def main():
 
     training_set = []
     labels = []
-    for website in session.query(Websites).all():
+    websites_query = list(session.query(Websites).filter(
+            Websites.male_ratio_alexa > 0)) # some websites do not have demographics
+    for website in websites_query:
         website_content = session.query(WebsitesContent).filter(
-            WebsitesContent.link==website.link)[0]
+                WebsitesContent.link==website.link)[0]
         words = website_content.words
         words = ' '.join(words)
 
@@ -67,13 +73,24 @@ def main():
         labels.append(gender)
 
     test_set = []
-    for url in get_history():
-        words = extract_words_from_url(url)
-        words = ' '.join(words)
+    test_set_classes = []
 
-        test_set.append(words)
+    if False:
+        for url in get_history():
+            words = extract_words_from_url(url)
+            words = ' '.join(words)
 
-    print classify(training_set, labels, test_set)
+            test_set.append(words)
+    else:
+        l = len(training_set)
+        print "Training set has %i items" % l
+        cut = int(l / 4 * 3)
+        test_set = training_set[cut:]
+        test_set_classes = labels[cut:]
+        training_set = training_set[0:cut]
+        labels = labels[0:cut]
+
+    print classify(training_set, labels, test_set, test_set_classes)
 
 
 if __name__ == '__main__':
